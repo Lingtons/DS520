d42efb2689e31f7547d4b89868e04fd8bd99e46e
Chima Nwadike <chimanwadike94@gmail.com>
2022-05-12 15:01:25 +0100

    initial

---
+++ DS-Project/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Clubx.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Clubx.Areas.Identity.IdentityHostingStartup))]
+namespace Clubx.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ DataAccess/IServices/IService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.IServices
+{
+    public interface IService<T>
+    {
+        IQueryable<T> GetAll();
+        T Get(Guid guid);
+        T Get(int id);
+        void Create(T obj);
+        void Update(T obj);
+        void Delete(T obj);
+    }
+}
